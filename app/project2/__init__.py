5565cd3cc283c158145906b007401cd270766e5e
Katrina <kat.kopio@gmail.com>
2020-11-05 20:06:48 +0800

    Made templates more jinjia-like

--- app/project2/__init__.py
+++ app/project2/__init__.py
@@ -5,4 +5,25 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://admin:azR4ENUNm1FPH4pj@project2.sqrmt.mongodb.net/Project2?retryWrites=true&w=majority"
 db = PyMongo(app)
 
+info = {
+    "total_distance": [
+        "Total Distance Travelled", 
+        "It was the first Nelvana-produced series to air on PBS before the Bookworm Bunch and Cyberchase. In 2017, on the 20th anniversary of the series' cancellation, a sequel series titled The Magic School Bus Rides Again premiered on Netflix."],
+    "speeding": [
+        "Speeding Violation Detector", 
+        "Miss Frizzle embarks on adventures with her class on the eponymous school bus. As they journey on their exciting field trips, they discover locations, creatures, time periods and more to learn about the wonders of science along the way."],
+    "geofencing": [
+        "Geofence Creation", 
+        "In 1994, The Magic School Bus concept was made into an animated series of the same name by Scholastic Entertainment and it premiered on September 10, 1994. The idea for the TV series was developed by former Scholastic Entertainment Vice President and Senior Editorial Director Craig Walker."],
+    "liveness": [
+        "Liveness Checking", 
+        "Scholastic Entertainment president Deborah Forte explained that adapting the books into an animated series was an opportunity to help kids \"learn about science in a fun way\".During this time, Forte had been hearing concerns from parents and teachers about how to improve science education for kids and minorities across the globe."],
+    "route_finding": [
+        "Route Finding", 
+        "When The Magic School Bus was syndicated on commercial networks, the Producer Says segment at the end of each episode was cut out to make space for commercials. The Producer Says segments were only seen when the series was shown on non-commercial networks, international networks, VHS, and DVD releases."],
+    "loop_counting": [
+        "Loop Counter", 
+        "Within the episodes, there were also time points where the episode fades out and then fades back in after a series of commercials are shown. On non-commercial networks, VHS, and DVD releases the scene immediately fades back in right after it fades out as no commercials are shown."]
+}
+
 from project2 import routes
--- app/project2/routes.py
+++ app/project2/routes.py
@@ -1,6 +1,5 @@
 from flask import render_template, url_for, flash, redirect, request
-from project2 import app, db
-
+from project2 import app, db, info
 # from project2.forms import 
 # from project2.models import 
 
@@ -11,28 +10,28 @@ def home():
 
 @app.route("/features")
 def features():
-    return render_template('features.html', title='Features')
+    return render_template('features.html', title='Features', info = info.items())
 
 @app.route("/features/total_distance")
 def total_distance():
-    return render_template('total_distance.html', title='Total Distance')
+    return render_template('total_distance.html', title='Total Distance', info = info.get("total_distance"))
 
 @app.route("/features/speeding")
 def speeding():
-    return render_template('speeding.html', title='Speeding')
+    return render_template('speeding.html', title='Speeding', info = info.get("speeding"))
 
 @app.route("/geofencing")
 def geofencing():
-    return render_template('geofencing.html', title='Geofencing')
+    return render_template('geofencing.html', title='Geofencing', info = info.get("geofencing"))
 
 @app.route("/liveness")
 def liveness():
-    return render_template('liveness.html', title='Liveness')
+    return render_template('liveness.html', title='Liveness', info = info.get("liveness"))
 
 @app.route("/route_finding")
 def route_finding():
-    return render_template('route_finding.html', title='Route Finding')
+    return render_template('route_finding.html', title='Route Finding', info = info.get("route_finding"))
 
 @app.route("/loop_counting")
 def loop_counting():
-    return render_template('loop_counting.html', title='Loop Counting')
+    return render_template('loop_counting.html', title='Loop Counting', info = info.get("loop_counting"))
