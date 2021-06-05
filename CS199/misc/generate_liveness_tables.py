c5ef0582912a75a20ce701d1e757a4375570c103
Katrina <kat.kopio@gmail.com>
2021-06-05 14:02:13 +0800

    Generate Liveness and Speed tables in LaTeX

---
+++ CS199/misc/generate_liveness_tables.py
@@ -0,0 +1,35 @@
+from api import check_liveness, parse_gpx_file
+
+def generate_latex(gps_data, time_limit, caption, label):
+    segments = check_liveness(gps_data, time_limit)
+    violations = segments.get("segments")
+    lst = []
+    for v in violations:
+        lst.append([v.get("liveness"), v.get("time1").strftime("%a, %d %b %Y"), v.get("time1").strftime("%H:%M:%S GMT"), v.get("time2").strftime("%a, %d %b"), v.get("time2").strftime("%Y %H:%M:%S GMT")])
+
+    print("\\begin{table}[ht]")
+    print("\\centering")
+    print("\\caption{",caption,"}",sep='')
+    print("\\begin{tabular}{|c|c|c|}")
+    print("\\hline")
+    print("\\textbf{Duration} & \\textbf{Start} & \\textbf{End} \\\\\hline")
+    for i in lst:
+        print(f"{i[0]} & {i[1]} & {i[3]} \\\\")
+        print(f"& {i[2]} & {i[4]} \\\\ \hline")
+    print("\\end{tabular}")
+    print("\\label{Tab:",label,"}",sep='')
+    print("\\end{table}")
+    print()
+
+if __name__ == '__main__':
+    dates = ["0420", "0421", "0422", "0424", "0425", "0426"]
+    filenames = [f"DS7-1-{dates[0]}", f"DS7-2-{dates[1]}", f"DS7-3-{dates[2]}", f"DS7-4-{dates[3]}", f"DS7-5-{dates[4]}", f"DS7-6-{dates[5]}", "ds1"]
+
+    for i in range(len(filenames)):
+        with open(f'../DS/{filenames[i]}.gpx', 'r') as gpx_file_location:
+            gps_data = parse_gpx_file(gpx_file_location)
+
+        time_limit = 1200 if i != 6 else 2
+        caption =  f"DS 2 {dates[i]} - Liveness Check" if i != 6 else "DS 1 Liveness Check"
+        label = f"{dates[i]}liveness" if i != 6 else "ds1liveness"
+        generate_latex(gps_data, time_limit, caption, label)
--- CS199/misc/generate_speed_tables.py
+++ CS199/misc/generate_speed_tables.py
@@ -10,7 +10,7 @@ def generate_latex(gps_data, caption, label, v_type, s, t):
 
     print("\\begin{table}[ht]")
     print("\\centering")
-    print("\\caption{",caption,"}")
+    print("\\caption{",caption,"}",sep='')
     print("\\begin{tabular}{|c|c|c|}")
     print("\\hline")
     print("\\textbf{Duration} & \\textbf{Start} & \\textbf{End} \\\\\hline")
@@ -18,30 +18,28 @@ def generate_latex(gps_data, caption, label, v_type, s, t):
         print(f"{i[0]} & {i[1]} & {i[3]} \\\\")
         print(f"& {i[2]} & {i[4]} \\\\ \hline")
     print("\\end{tabular}")
-    print("\\label{Tab:",label,"}")
+    print("\\label{Tab:",label,"}",sep='')
     print("\\end{table}")
+    print()
 
 if __name__ == '__main__':
-    # filename = f"ds1"
-    date = "0426"
-    filename = f"DS7-6-{date}"
-    with open(f'../../DS/{filename}.gpx', 'r') as gpx_file_location:
-        gps_data = parse_gpx_file(gpx_file_location)
+    dates = ["0420", "0421", "0422", "0424", "0425", "0426"]
+    filenames = [f"DS7-1-{dates[0]}", f"DS7-2-{dates[1]}", f"DS7-3-{dates[2]}", f"DS7-4-{dates[3]}", f"DS7-5-{dates[4]}", f"DS7-6-{dates[5]}", "ds1"]
 
-    s = 60
-    t = 0.5
+    for i in range(len(filenames)):
+        with open(f'../DS/{filenames[i]}.gpx', 'r') as gpx_file_location:
+            gps_data = parse_gpx_file(gpx_file_location)
 
-    v_type = "Explicit"
-    # caption = f"Dataset 1 - {v_type}"
-    # label = "ds1speedingexplicit"
-    caption = f"DS2 {date} - {v_type}"
-    label = f"{date}speedingexplicit"
-    generate_latex(gps_data, caption, label, v_type, s, t)
-    print()
+        s = 60 if i != 6 else 30
+        t = 0.5
+
+        v_type = "Explicit"
+        caption =  f"DS 2 {dates[i]} - {v_type}" if i != 6 else f"Dataset 1 - {v_type}"
+        label = f"{dates[i]}speedingexplicit" if i != 6 else "ds1speedingexplicit"
+        generate_latex(gps_data, caption, label, v_type, s, t)
     
-    # v_type = "Location"
-    # caption = f"Dataset 1 - {v_type}"
-    # label = "ds1speedinglocation"
-    caption = f"DS2 {date} - {v_type}"
-    label = f"{date}speedinglocation"
-    generate_latex(gps_data, caption, label, v_type, s, t)
+        v_type = "Location"
+        caption =  f"DS 2 {dates[i]} - {v_type}" if i != 6 else f"Dataset 1 - {v_type}"
+        label = f"{dates[i]}speedinglocation" if i != 6 else "ds1speedinglocation"
+        generate_latex(gps_data, caption, label, v_type, s, t)
+        print()
